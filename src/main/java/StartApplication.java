4976a7071a822038a720e378f7d3db497104cc7f
taykita <nordac2001@gmai.com>
2022-11-26 19:46:11 +0300

    Поменял публичный и приватный ключи местами

--- src/main/java/StartApplication.java
+++ src/main/java/StartApplication.java
@@ -4,10 +4,9 @@ import org.slf4j.LoggerFactory;
 import javax.crypto.*;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
 import java.util.Arrays;
 
 public class StartApplication {
@@ -26,7 +25,7 @@ public class StartApplication {
     }
 
     private static void signCheck(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        logger.debug("Пришел запрос на проверку документа по пути {} с подписью по пути {} и публичным ключом {}", args[0], args[1], args[2]);
+        logger.debug("Пришел запрос на проверку документа по пути {} с подписью по пути {} и приватным ключом {}", args[0], args[1], args[2]);
 
         byte[] stringDoc = FileUtil.getBytesFromFile(args[0]);
 
@@ -35,20 +34,20 @@ public class StartApplication {
 
         byte[] stringSign = FileUtil.getBytesFromFile(args[1]);
 
-        String publicKeyString = FileUtil.getStringFromFile(args[2]);
-        String modulus = publicKeyString.substring(publicKeyString.indexOf("modulus: "));
-        String pubExp = modulus.substring(modulus.indexOf("public exponent: ")).split(" ")[2];
-        modulus = publicKeyString.substring(publicKeyString.indexOf("modulus: ")).split(" ")[1].split("\n")[0];
+        String privateKeyString = FileUtil.getStringFromFile(args[2]);
+        String modulus = privateKeyString.substring(privateKeyString.indexOf("modulus: "));
+        String pubExp = modulus.substring(modulus.indexOf("private exponent: ")).split(" ")[2];
+        modulus = privateKeyString.substring(privateKeyString.indexOf("modulus: ")).split(" ")[1].split("\n")[0];
         logger.trace(modulus);
         logger.trace(pubExp);
 
-        RSAPublicKeySpec spec = new RSAPublicKeySpec(new BigInteger(modulus, 10), new BigInteger(pubExp, 10));
+        RSAPrivateKeySpec spec = new RSAPrivateKeySpec(new BigInteger(modulus, 10), new BigInteger(pubExp, 10));
         KeyFactory kf = KeyFactory.getInstance("RSA");
-        PublicKey publicKey = kf.generatePublic(spec);
-        logger.trace(publicKey.toString());
+        PrivateKey privateKey = kf.generatePrivate(spec);
+        logger.trace(privateKey.toString());
 
         Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
         byte[] result = cipher.doFinal(stringSign);
         logger.trace(Arrays.toString(result));
 
@@ -75,20 +74,20 @@ public class StartApplication {
         generator.initialize(2048);
         KeyPair keyPair = generator.generateKeyPair();
         Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
+        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
         byte[] data = cipher.doFinal(encodedHash);
 
         String docHash = bytesToHex(data);
         logger.debug("Цифровая подпись документа - {}", docHash);
 
         FileUtil.createFileFromBytes(data, args[0] + ".sign");
-        FileUtil.createFileFromString(keyPair.getPublic().toString(), args[0] + ".publicKey");
+        FileUtil.createFileFromString(keyPair.getPrivate().toString(), args[0] + ".privateKey");
 
         System.out.println("===================================================================");
         System.out.println("====================Документ подписан успешно!=====================");
         System.out.println("===================================================================");
         System.out.println(args[0] + ".sign - подпись");
-        System.out.println(args[0] + ".publicKey - публичный ключ");
+        System.out.println(args[0] + ".privateKey - приватный ключ");
     }
 
 
