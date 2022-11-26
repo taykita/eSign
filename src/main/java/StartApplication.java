d15d6a8713e32e54d002d875c964eb2f8d100bb7
taykita <nordac2001@gmai.com>
2022-11-26 19:20:17 +0300

    Небольшие улучшения.

--- src/main/java/StartApplication.java
+++ src/main/java/StartApplication.java
@@ -8,64 +8,65 @@ import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
+import java.util.Arrays;
 
 public class StartApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(StartApplication.class);
 
-    private static final int L = 576;
-
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException {
         if (args.length == 2) {
             signing(args);
         } else if (args.length == 4) {
-            logger.debug("Пришел запрос на проверку документа по пути {} с подписью по пути {} и публичным ключом {}", args[0], args[1], args[2]);
-
-            String stringDoc = FileUtil.getStringFromFile(args[0]);
-
-            MessageDigest digest = MessageDigest.getInstance(args[3]);
-            byte[] encodedHash = digest.digest(stringDoc.getBytes(StandardCharsets.UTF_8));
-            String currentDocHash = bytesToHex(encodedHash);
-
-            byte[] stringSign = FileUtil.getBytesFromFile(args[1]);
-
-            String publicKeyString = FileUtil.getStringFromFile(args[2]);
-            String modulus = publicKeyString.substring(publicKeyString.indexOf("modulus: "));
-            String pubExp = modulus.substring(modulus.indexOf("public exponent: ")).split(" ")[2];
-            modulus = publicKeyString.substring(publicKeyString.indexOf("modulus: ")).split(" ")[1].split("\n")[0];
-            logger.trace(modulus);
-            logger.trace(pubExp);
-
-            RSAPublicKeySpec spec = new RSAPublicKeySpec(new BigInteger(modulus, 10), new BigInteger(pubExp, 10));
-            KeyFactory kf = KeyFactory.getInstance("RSA");
-            PublicKey publicKey = kf.generatePublic(spec);
-            logger.trace(publicKey.toString());
-
-            Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.DECRYPT_MODE, publicKey);
-            byte[] result = cipher.doFinal(stringSign);
-            String trustDocHash = bytesToHex(result);
-            logger.trace(trustDocHash);
-
-            if (trustDocHash.equals(currentDocHash)) {
-                System.out.println("Документ подписан верно!");
-            } else {
-                System.out.println("Подпись неверна или изменен состав документа!");
-            }
+            signCheck(args);
         } else {
             throw new IllegalArgumentException("Ошибка обработки входных данных. Неверное количество аргументов");
         }
 
     }
 
+    private static void signCheck(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        logger.debug("Пришел запрос на проверку документа по пути {} с подписью по пути {} и публичным ключом {}", args[0], args[1], args[2]);
+
+        byte[] stringDoc = FileUtil.getBytesFromFile(args[0]);
+
+        MessageDigest digest = MessageDigest.getInstance(args[3]);
+        byte[] encodedHash = digest.digest(stringDoc);
+
+        byte[] stringSign = FileUtil.getBytesFromFile(args[1]);
+
+        String publicKeyString = FileUtil.getStringFromFile(args[2]);
+        String modulus = publicKeyString.substring(publicKeyString.indexOf("modulus: "));
+        String pubExp = modulus.substring(modulus.indexOf("public exponent: ")).split(" ")[2];
+        modulus = publicKeyString.substring(publicKeyString.indexOf("modulus: ")).split(" ")[1].split("\n")[0];
+        logger.trace(modulus);
+        logger.trace(pubExp);
+
+        RSAPublicKeySpec spec = new RSAPublicKeySpec(new BigInteger(modulus, 10), new BigInteger(pubExp, 10));
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = kf.generatePublic(spec);
+        logger.trace(publicKey.toString());
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        byte[] result = cipher.doFinal(stringSign);
+        logger.trace(Arrays.toString(result));
+
+        if (Arrays.equals(result, encodedHash)) {
+            System.out.println("Документ подписан верно!");
+        } else {
+            System.out.println("Подпись неверна или изменен состав документа!");
+        }
+    }
+
     private static void signing(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         logger.debug("Пришел запрос на шифрование документа по пути {}", args[0]);
 
-        String stringDoc = FileUtil.getStringFromFile(args[0]);
-        logger.debug("Пришел документ на подпись. Его текст - \n{}", stringDoc);
+        byte[] stringDoc = FileUtil.getBytesFromFile(args[0]);
+        logger.debug("Пришел документ на подпись", stringDoc);
 
         MessageDigest digest = MessageDigest.getInstance(args[1]);
-        byte[] encodedHash = digest.digest(stringDoc.getBytes(StandardCharsets.UTF_8));
+        byte[] encodedHash = digest.digest(stringDoc);
 
         String trustDocHash = bytesToHex(encodedHash);
         logger.debug("Вычислен хэш документа - {}", trustDocHash);
